83888552e049d880c2c94ed3fa6cd30e316c3a52
Osama Abdelaziz <osama.abdelaziz23@gmail.com>
2022-08-24 09:04:23 +0200

    build onboarding screen with mvvm

---
+++ lib/presentation/base/baseviewmodel.dart
@@ -0,0 +1,12 @@
+
+// By this we can ovveride any function inside these abstract classes
+abstract class BaseViewModel extends BaseViewModelInputs with BaseViewModelOutputs{
+  // shared variables and functions that will be used through any view model.
+}
+
+abstract class BaseViewModelInputs {
+  void start(); // will be called while init. of view model
+  void dispose(); // will be called when viewmodel dies.
+}
+
+abstract class BaseViewModelOutputs {}
